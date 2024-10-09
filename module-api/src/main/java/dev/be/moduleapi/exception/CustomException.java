01cfed47a14805a612b80d99cce729c71a27bd8f
lby132 <quddbscjswow@naver.com>
2024-10-09 17:55:46 +0900

    exception 핸들링

--- module-api/src/main/java/dev/be/moduleapi/exception/CustomException.java
+++ module-api/src/main/java/dev/be/moduleapi/exception/CustomException.java
@@ -1,4 +1,22 @@
 package dev.be.moduleapi.exception;
 
-public class CustomException {
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomException extends RuntimeException {
+    private String returnCode;
+    private String returnMessage;
+
+    public CustomException(CodeEnum codeEnum) {
+        super(codeEnum.getMessage());
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+    }
 }
--- module-api/src/main/java/dev/be/moduleapi/response/CommonResponse.java
+++ module-api/src/main/java/dev/be/moduleapi/response/CommonResponse.java
@@ -1,4 +1,34 @@
 package dev.be.moduleapi.response;
 
-public class CommonResponse {
+import dev.be.modulecommon.enums.CodeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommonResponse<T> {
+    private String returnCode;
+    private String returnMessage;
+    private T info;
+
+    public CommonResponse(CodeEnum codeEnum) {
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+    }
+
+    public CommonResponse(T info) {
+        setReturnCode(CodeEnum.SUCCESS.getCode());
+        setReturnMessage(CodeEnum.SUCCESS.getMessage());
+        setInfo(info);
+    }
+
+    public CommonResponse(CodeEnum codeEnum,  T info) {
+        setReturnCode(codeEnum.getCode());
+        setReturnMessage(codeEnum.getMessage());
+        setInfo(info);
+    }
 }
