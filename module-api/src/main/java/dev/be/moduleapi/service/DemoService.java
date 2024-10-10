314234e61e6b32d8ce3e504adcc46d1deded841d
lby132 <quddbscjswow@naver.com>
2024-10-10 18:37:19 +0900

    multi module에서 db 연동

--- module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
+++ module-api/src/main/java/dev/be/moduleapi/service/DemoService.java
@@ -1,6 +1,9 @@
 package dev.be.moduleapi.service;
 
+import dev.be.moduleapi.exception.CustomException;
+import dev.be.modulecommon.domain.Member;
 import dev.be.modulecommon.enums.CodeEnum;
+import dev.be.modulecommon.repositories.MemberRepository;
 import dev.be.modulecommon.service.CommonDemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +14,12 @@ public class DemoService {
 
     private final CommonDemoService commonDemoService;
 
+    private final MemberRepository memberRepository;
+
     public String save() {
+        memberRepository.save(Member.builder()
+                .name(Thread.currentThread().getName())
+                .build());
         System.out.println(CodeEnum.SUCCESS.getCode());
         System.out.println(commonDemoService.commonService());
         return "save";
@@ -19,5 +27,12 @@ public class DemoService {
 
     public String find() {
         return "find";
+    }
+
+    public String exception() {
+        if (true) {
+            throw new CustomException(CodeEnum.UNKNOWN_ERROR);
+        }
+        return "exception";
     }
 }
---
+++ module-common/src/main/java/dev/be/modulecommon/domain/Member.java
@@ -0,0 +1,20 @@
+package dev.be.modulecommon.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
